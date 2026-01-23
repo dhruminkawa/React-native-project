0dcde144e5772d54a93b99e53c8e1b2a6994096b
dhrumin kawa <dhruminkawa6@gmail.com>
2026-01-23 18:19:43 +0530

    landing page splashscreen

--- src/features/auth/SplashScreen.tsx
+++ src/features/auth/SplashScreen.tsx
@@ -1,10 +1,37 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import React from 'react';
 import { splashStyles } from '@unistyles/authStyles';
+import Animated, { FadeInDown } from 'react-native-reanimated';
+import CustomText from '@components/global/CustomText';
+
+
 const SplashScreen = () => {
   return (
     <View style={splashStyles.container}>
-      <Text style={splashStyles.msgText}>SplashScreen</Text>
+      {/* <Text style={splashStyles.msgText}>SplashScreen</Text> */}
+      <Image
+        source={require('@assets/images/logo_t.png')}
+        style={splashStyles.logoImage}
+      />
+      <Animated.View style={splashStyles.animatedContainer}
+      entering={FadeInDown.delay(400).duration(800)}
+      >
+        <Image
+          source={require('@assets/images/tree.png')}
+          style={splashStyles.treeImage}
+        />
+         
+         <CustomText
+        variant='h5'
+        style={splashStyles.msgText}
+        fontFamily='Okra-Medium'
+        color='#fff'
+         >
+          From Kitchen to doorstep, your cravings, delivered!
+         </CustomText>
+          
+
+      </Animated.View>
     </View>
   );
 };
